9b13e26f5f8cc34379ab5e023a1905d1241ea93f
Muhammad Fajar <mhddfajar@gmail.com>
2024-07-22 23:04:51 +0700

    Todolist App VueJS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-black">
+    <h1 class="text-4xl font-bold text-white">Hello, Tailwind CSS with Vite!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
